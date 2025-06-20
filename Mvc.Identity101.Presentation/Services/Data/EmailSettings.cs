29b485cbdc094715ab1c23108bff1665155cc80b
xxRyuka <enesfurkankus16@gmail.com>
2025-06-20 18:58:36 +0300

    forgot pw and email service implemented

---
+++ Mvc.Identity101.Presentation/Services/Data/EmailSettings.cs
@@ -0,0 +1,8 @@
+namespace Mvc.Identity101.Services.Data.Dto;
+
+public class EmailSettings
+{
+    public string SmtpHost { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+}
